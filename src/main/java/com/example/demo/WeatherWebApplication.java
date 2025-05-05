cee49f9f62c5bf295d0716501d640974804e466e
Flapx24 <bares.gafer23@cadiz.salesianos.edu>
2025-05-05 10:27:34 +0200

    Implement locale support and fix weather forecast dates display

--- src/main/java/com/example/demo/WeatherWebApplication.java
+++ src/main/java/com/example/demo/WeatherWebApplication.java
@@ -2,12 +2,38 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
-public class WeatherWebApplication {
+public class WeatherWebApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherWebApplication.class, args);
 	}
 
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+		sessionLocaleResolver.setDefaultLocale(Locale.of("es", "ES"));
+		return sessionLocaleResolver;
+	}
+
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+		LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+		lci.setParamName("lang");
+		return lci;
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(localeChangeInterceptor());
+	}
 }
